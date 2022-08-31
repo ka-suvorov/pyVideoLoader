c7bb88de5d631266616ab9670228605bb0f01c3f
Konstantin Suvorov <91534415+ka-suvorov@users.noreply.github.com>
2022-08-31 18:23:54 +0300

    add feature for video download function:
    The skip-unavailable-fragments feature has been added to the download function to prevent some video download errors.

--- GVDownloader.py
+++ GVDownloader.py
@@ -37,13 +37,13 @@ def get_folder():
 
 
 def video_load():
-	task = 10
-	x = 0
+	
 	_path = get_path.get()
 	_path += '\\%(title)s.%(ext)s'
 	_url = url_video.get()
 	ydl_opts = {
-		'format': 'bestvideo[ext=mp4]+bestaudio[ext=mp4]/mp4+best[height<=1440]',
+		'format': 'bestvideo[ext=mp4]+bestaudio[ext=mp4]/'
+		'mp4+best[height>=1440]/(bv*[vcodec~=\'^((he|a)vc|h26[45])\']+ba) / (bv*+ba/b)',
 		'playlist': 'yes',
 		'ignore-errors': True,
 		'live-from-start': True,
@@ -51,6 +51,7 @@ def video_load():
 		'limit-rate': '1000M',
 		'force-overwrites': True,
 		'continue_dl': True,
+		'skip-unavailable-fragments': True,
 	}
 	with yt_dlp.YoutubeDL(ydl_opts) as ydl:
 		ydl.download([_url])
@@ -60,14 +61,14 @@ def video_load():
 
 
 def open_video_source():
-	programName = "notepad.exe"
-	fileName = "videoservice_txt"
-	sp.Popen([programName, fileName])
+	programname = "notepad.exe"
+	filename = "videoservice_txt"
+	sp.Popen([programname, filename])
 
 
 # Vars
 column = ['Progress info']
-version = '0.0.1'
+version = '0.1.0'
 url_link = ''
 forder = ''
 url = ''
@@ -92,11 +93,9 @@ root.config(bg='#AFEEEE')
 labels_frame = ttk.LabelFrame(root)
 labels_frame.pack(fill=BOTH, expand=1)
 
-gvd_label = ttk.Label(labels_frame,
-                      text='GVDownloader -version 0.0.1!')
+gvd_label = ttk.Label(labels_frame, text='GVDownloader -version 0.1.0!')
 gvd_label.pack(pady=1, fill=X, side=TOP)
-info_label = ttk.Label(labels_frame,
-                       text='Download videos, playlist, streams from many resources!')
+info_label = ttk.Label(labels_frame, text='Download videos, playlist, streams from many resources!')
 info_label.pack(pady=1, fill=X, side=TOP)
 
 path_frame = ttk.LabelFrame(root)
@@ -105,9 +104,7 @@ path_frame.pack(fill=BOTH, expand=1)
 get_path = ttk.Entry(path_frame, width=48)
 get_path.pack(pady=1, side=LEFT)
 
-path_button = ttk.Button(path_frame,
-                         text="выбрать каталог для загрузки", width=32,
-                         command=get_folder)
+path_button = ttk.Button(path_frame, text="выбрать каталог для загрузки", width=32, command=get_folder)
 path_button.pack(pady=1, side=LEFT)
 
 urls_frame = ttk.LabelFrame(root)
@@ -116,41 +113,33 @@ urls_frame.pack(fill=BOTH, expand=1)
 url_video = ttk.Entry(urls_frame, width=48)
 url_video.pack(pady=1, side=LEFT)
 
-url_button = ttk.Button(urls_frame,
-                        text='Вставьте ссылку из буфера обмена', width=32,
-                        command=get_url)
+url_button = ttk.Button(urls_frame, text='Вставьте ссылку из буфера обмена', width=32, command=get_url)
 url_button.pack(pady=1, side=LEFT)
 
 videoservice_frame = ttk.LabelFrame(root)
 videoservice_frame.pack(fill=BOTH, expand=1)
 
-videoservice_button = ttk.Button(videoservice_frame,
-                             text='Посмотреть список видеосервисов',
-                             command=open_video_source)
+videoservice_button = ttk.Button(videoservice_frame, text='Посмотреть список видеосервисов', command=open_video_source)
 videoservice_button.pack(pady=2, fill=X, side=BOTTOM)
 
 download_frame = ttk.LabelFrame(root)
 download_frame.pack(fill=BOTH, expand=1)
 
-download_button = ttk.Button(download_frame,
-                             text='Скачать видео. плейлист или живой стрим',
-                             command=video_load)
+download_button = ttk.Button(download_frame, text='Скачать видео. плейлист или живой стрим', command=video_load)
 download_button.pack(pady=2, fill=X, side=BOTTOM)
 
 footer_frame = ttk.LabelFrame(root)
 footer_frame.pack(fill=BOTH, expand=1)
 
-exit_button = ttk.Button(footer_frame,
-                             text='Выйти из программы',
-                             command=exit_application)
+exit_button = ttk.Button(footer_frame, text='Выйти из программы', command=exit_application)
 exit_button.pack(pady=2, fill=X, side=TOP)
 
 
 statusbar = ttk.Entry(footer_frame)
 statusbar.pack(side=BOTTOM, fill=X)
 
-l = ttk.Style()
-l.configure('TLabel', font=('Arial', 14, 'bold'), padding=2, foreground='#A52A2A')
+tl = ttk.Style()
+tl.configure('TLabel', font=('Arial', 14, 'bold'), padding=2, foreground='#A52A2A')
 e = ttk.Style()
 e.configure('TEntry', font=('Arial', 14, 'bold'), padding=4)
 b = ttk.Style()
--- lin_pytube.py
+++ lin_pytube.py
@@ -1,7 +1,8 @@
 import functions
 import yt_dlp
 from colorama import init
-from colorama import Fore, Back, Style
+from colorama import Fore, Style
+
 init()
 
 # Variables
@@ -21,13 +22,17 @@ def download():
     link_url.strip()
     global folder
     ydl_opts = {
-        'format': 'bestvideo[ext=mp4]+bestaudio[ext=mp4]/mp4+best[height<=1440]/(bv*[vcodec~=\'^((he|a)vc|h26[45])\']+ba) / (bv*+ba/b)',
+        'format': 'bestvideo[ext=mp4]+bestaudio[ext=mp4]/mp4+best[height>=1440]/'
+        '(bv*[vcodec~=\'^((he|a)vc|h26[45])\']+ba) / (bv*+ba/b)',
         'playlist': 'yes',
         'ignore-errors': True,
         'live-from-start': True,
         'outtmpl': folder,
         'limit-rate': '1000M',
-    }
+        'force-overwrites': True,
+        'continue_dl': True,
+        'skip-unavailable-fragments': True,
+        }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([link_url])
         functions.clearscreen()
